32fece1ab2445665bd55e73cd84c05e689acc017
CorrVec <155963243+CorrVec@users.noreply.github.com>
2024-11-27 15:39:01 +0100

    Update json-splitter.sh

--- utils/json-splitter.sh
+++ utils/json-splitter.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+# Estrai il contenuto di repositories dal file json
+repositories=$(jq -r '.repositories[]' repo.json)
+
+# Conta il numero totale di elementi
+total=$(echo "$repositories" | wc -l)
+
+# Calcola il punto di divisione
+split=$(( (total + 1) / 2 ))
+
+# Dividi in due parti
+list1=$(echo "$repositories" | head -n "$split")
+list2=$(echo "$repositories" | tail -n +$((split + 1)))
+
+# Crea due file json temporanei
+echo "$list1" > tmp/repo_list1.json
+echo "$list2" > tmp/repo_list2.json
