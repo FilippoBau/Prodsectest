c47828fc2d0ea0fb33fc55e1a4e88e259ef27562
CorrVec <155963243+CorrVec@users.noreply.github.com>
2025-02-25 10:23:13 +0100

    Update json_repository_validator.sh

--- utils/splitter/json_repository_validator.sh
+++ utils/splitter/json_repository_validator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Estrai la lunghezza dell'array "repositories" con jq
-echo "$1"
-count=$(echo "$1" | jq '.repositories | length')
+cat $1
+count=$(cat $1 | jq '.repositories | length')
 
 # Controlla se ci sono almeno 1 elemento
 if [[ "$count" -le 1 ]]; then
